94faf130b63ff7847c6aa9a86c7ca43775750e10
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2023-10-14 16:32:28 -0400

    update

---
+++ experiment_utah/gateway_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function check {
+  STATUS=`lsof -Pi :8080 -sTCP:LISTEN -t`
+  if [ "$STATUS" = "" ]; then
+    return 1
+  fi
+  return 0
+}
+
+until check; do
+  echo "Waiting for gateway to be ready"
+  sleep 5
+done
+
+echo "gateway ready"
+
