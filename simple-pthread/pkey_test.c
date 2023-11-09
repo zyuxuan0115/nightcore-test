b80ab6952fcb060ae135852374ace715485de972
zyuxuan <zyuxuan@ms1211.utah.cloudlab.us>
2023-11-09 12:54:59 -0700

    update

--- simple-pthread/pkey_test.c
+++ simple-pthread/pkey_test.c
@@ -3,9 +3,12 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
-#include "mpt/mpt.h"
-#include "mpt/hash.h"
-#include "mpt/pkey.h"
+#include <errno.h>
+#include <mpt/mpt.h>
+#include <mpt/hash.h>
+#include <mpt/pkey.h>
+
+#define MMAP_PAGE_SIZE 4*1024 
 
 void *functionMutex(void*);
 void function();
@@ -13,11 +16,24 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 clock_t start;
 
 main(){
+   int pkey = pkey_alloc(0, PKEY_DISABLE_ACCESS);
+   if (pkey == ENOSPC) {
+      printf("No available keys\n");
+   }
+   void* ptr = mmap(NULL, MMAP_PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+   if (ptr==NULL){
+      printf("mmap fails\n");
+   }
+   // memory cannot be access at all in main thread
+   int ret = pkey_mprotect(ptr, MMAP_PAGE_SIZE, PROT_NONE, pkey); 
+
+   int* ptr_int = (int*) ptr;
+   *ptr_int  = 5;
    // creating thread (mutex version)
    int rc;
    pthread_mutex_lock(&mutex);
    pthread_t thread;
-   if( (rc = pthread_create( &thread, NULL, &functionMutex, NULL)) ){
+   if( (rc = pthread_create( &thread, NULL, &functionMutex, ptr)) ){
       printf("Thread creation failed: %d\n", rc);
    }
    // do something 
@@ -30,9 +46,11 @@ main(){
    exit(0);
 }
 
-void *functionMutex(void*){
+void *functionMutex(void* arg){
    pthread_mutex_lock( &mutex );
    // do something (serverless function)
+   int* ptr_int = (int*) arg;
+   *ptr_int = 5;
    pthread_mutex_unlock( &mutex);
    clock_t end = clock();
    float seconds = (float)(end - start) / CLOCKS_PER_SEC;
