ebe34da2f2a973000e0a8a340577e2edc1b40a5a
zyuxuan <zyuxuan@localhost>
2023-11-14 13:48:06 -0700

    update

--- simple-pthread/detect_call_stack/sort.c
+++ simple-pthread/detect_call_stack/sort.c
@@ -5,6 +5,36 @@
 #define TIMES 20
 static struct timeval tm1;
 
+void bubble_sort(int* a, int n);
+void bubble_sort2(int* a, int n);
+void bubble_sort3(int* a, int n);
+void* sort_array(void*);
+
+int main(){
+   printf("[sort] bubble sort is done\n");
+   return 0;
+}
+
+
+void* sort_array(void* arg) {
+   int data[ARRAY_LEN], j=0;
+   while (j<TIMES){
+      for(int i=0; i<ARRAY_LEN; ++i){
+         data[i] = rand();
+      }
+      bubble_sort2(data, ARRAY_LEN);
+      for(int i=0; i<ARRAY_LEN; ++i){
+         data[i] = rand();
+      }
+      bubble_sort(data, ARRAY_LEN);
+      for(int i=0; i<ARRAY_LEN; ++i){
+         data[i] = rand();
+      }
+      bubble_sort3(data, ARRAY_LEN);
+         j++;
+   }
+}
+
 void bubble_sort (int *a, int n) {
    int i, t, s = 1;
    while (s) {
@@ -44,7 +74,4 @@ void bubble_sort3 (int *a, int n) {
    }
 }
 
-int main(){
-   printf("[sort] bubble sort is done\n");
-   return 0;
-}
+
