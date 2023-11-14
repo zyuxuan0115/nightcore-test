d54c2d75124c6d0495a413f42eb852cadac1e3a3
zyuxuan <zyuxuan@localhost>
2023-11-14 13:03:33 -0700

    update

--- simple-pthread/detect_call_stack/sort.c
+++ simple-pthread/detect_call_stack/sort.c
@@ -5,74 +5,46 @@
 #define TIMES 20
 static struct timeval tm1;
 
-
 void bubble_sort (int *a, int n) {
-    int i, t, s = 1;
-    while (s) {
-        s = 0;
-        for (i = 1; i < n; i++) {
-            if (a[i] < a[i - 1]) {
-                t = a[i];
-                a[i] = a[i - 1];
-                a[i - 1] = t;
-                s = 1;
-            }
-        }
-    }
+   int i, t, s = 1;
+   while (s) {
+      s = 0;
+      for (i = 1; i < n; i++) {
+         if (a[i] < a[i - 1]) {
+            t = a[i];
+            a[i] = a[i - 1];
+            a[i - 1] = t;
+            s = 1;
+         }
+      }
+   }
 }
 
 void bubble_sort2 (int *a, int n) {
-	for (int i = 0; i<n; i++){
-		for (int j=i+1; j<n; j++){
-			if (a[j] < a[i]){
-				int tmp = a[i];
-				a[i] = a[j];
-				a[j] = tmp;
-			}
-		}
-	}
+   for (int i = 0; i<n; i++){
+      for (int j=i+1; j<n; j++){
+         if (a[j] < a[i]){
+            int tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+         }
+      }
+   }
 }
-
-
 
 void bubble_sort3 (int *a, int n) {
-	for (int i = 0; i<n; i++){
-		for (int j=i+1; j<n; j++){
-			if (a[j] < a[i]){
-				int tmp = a[i];
-				a[i] = a[j];
-				a[j] = tmp;
-			}
-		}
-	}
-}
-
-
-
-
-void sort_array() {
-    int data[ARRAY_LEN], j=0;
-		while (j<TIMES){
-			for(int i=0; i<ARRAY_LEN; ++i){
-        data[i] = rand();
-			}
-			bubble_sort2(data, ARRAY_LEN);
-			for(int i=0; i<ARRAY_LEN; ++i){
-				data[i] = rand();
-			}
-			bubble_sort(data, ARRAY_LEN);
-			for(int i=0; i<ARRAY_LEN; ++i){
-				data[i] = rand();
-			}
-			bubble_sort3(data, ARRAY_LEN);
-
-			j++;
-		}
-
+   for (int i = 0; i<n; i++){
+      for (int j=i+1; j<n; j++){
+         if (a[j] < a[i]){
+            int tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+         }
+      }
+   }
 }
 
 int main(){
-    sort_array();
-		printf("[sort] bubble sort is done\n");
-    return 0;
+   printf("[sort] bubble sort is done\n");
+   return 0;
 }
