d33ba41d3919414119909351746b69208272dfb3
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2023-11-23 18:53:17 -0500

    update

--- MergeFunc/build.sh
+++ MergeFunc/build.sh
@@ -1,6 +1,7 @@
 NIGHTCORE_PATH=/proj/zyuxuanssf-PG0/nightcore
 LLVM_PATH=/proj/zyuxuanssf-PG0/llvm-project
 
+rm *.so *.o *.bc *.ll *.s
 clang -I$NIGHTCORE_PATH/include -fPIC -emit-llvm foo.c -c -o foo.bc
 clang -I$NIGHTCORE_PATH/include -fPIC -emit-llvm bar.c -c -o bar.bc
 opt -load $LLVM_PATH/build/lib/LLVMMergeFunc.so -enable-new-pm=0 -ChangeFuncName bar.bc -o bar_func_only.bc
