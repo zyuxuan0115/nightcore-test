e30750c670c9a3595c3c2ab7c01e3629912e2398
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2023-11-25 14:42:47 -0500

    update

--- MergeFunc/build.sh
+++ MergeFunc/build.sh
@@ -7,7 +7,8 @@ rm *.so *.o *.ll
 clang -I$NIGHTCORE_PATH/include -fPIC -emit-llvm -S $NIGHTCORE_PATH/examples/c/foo.c -c -o foo.ll
 clang -I$NIGHTCORE_PATH/include -fPIC -emit-llvm -S $NIGHTCORE_PATH/examples/c/bar.c -c -o bar.ll
 opt -load $LLVM_PATH/build/lib/LLVMMergeFunc.so -enable-new-pm=0 -ChangeFuncName bar.ll -S -o bar_only.ll
-llvm-link foo.ll bar_only.ll -o foo_bar.ll -S
-opt -load $LLVM_PATH/build/lib/LLVMMergeFunc.so -S -enable-new-pm=0 -o new_foo.ll -ConvertRPC2NormalCall < foo_bar.ll
+llvm-link foo.ll bar_only.ll -o foo_rpc_bar.ll -S
+opt -load $LLVM_PATH/build/lib/LLVMMergeFunc.so -S -enable-new-pm=0 -o foo_bar.ll -ConvertRPC2NormalCall < foo_rpc_bar.ll
+opt -load $LLVM_PATH/build/lib/LLVMMergeFunc.so -S -enable-new-pm=0 -o new_foo.ll -ChangeCalleeFunc < foo_bar.ll
 llc -filetype=obj -relocation-model=pic new_foo.ll -o libfoo.o
 clang -shared -fPIC -O2 -I../../include libfoo.o -o libfoo.so 
