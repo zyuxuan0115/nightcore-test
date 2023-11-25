234ff0fa843aa95c54c3c0371944061bacee8ede
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2023-11-25 11:46:28 -0500

    update

--- MergeFunc/build.sh
+++ MergeFunc/build.sh
@@ -6,8 +6,8 @@ LLVM_PATH=/proj/zyuxuanssf-PG0/llvm-project
 rm *.so *.o *.ll
 clang -I$NIGHTCORE_PATH/include -fPIC -emit-llvm -S $NIGHTCORE_PATH/examples/c/foo.c -c -o foo.ll
 clang -I$NIGHTCORE_PATH/include -fPIC -emit-llvm -S $NIGHTCORE_PATH/examples/c/bar.c -c -o bar.ll
-opt -load $LLVM_PATH/build/lib/LLVMMergeFunc.so -enable-new-pm=0 -ChangeFuncName bar.ll -S -o bar_func_only.ll
-llvm-link foo.ll bar_func_only.ll -o foo_bar.ll -S
+opt -load $LLVM_PATH/build/lib/LLVMMergeFunc.so -enable-new-pm=0 -ChangeFuncName bar.ll -S -o bar_only.ll
+llvm-link foo.ll bar_only.ll -o foo_bar.ll -S
 opt -load $LLVM_PATH/build/lib/LLVMMergeFunc.so -S -enable-new-pm=0 -o new_foo.ll -ConvertRPC2NormalCall < foo_bar.ll
 llc -filetype=obj -relocation-model=pic new_foo.ll -o libfoo.o
 clang -shared -fPIC -O2 -I../../include libfoo.o -o libfoo.so 
