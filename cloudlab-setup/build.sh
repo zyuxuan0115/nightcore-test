abf632fd10bccde341275e0bf8ef963f4984938d
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2023-11-27 17:36:29 -0500

    update

--- cloudlab-setup/build.sh
+++ cloudlab-setup/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cp bashrc ~/.bashrc 
 source ~/.bashrc
+cp id_ed25519 ~/.ssh/
+cp id_ed25519.pub ~/.ssh/
+chmod 644 ~/.ssh/id_ed25519.pub
+chmod 600 ~/.ssh/id_ed25519
+cat id_ed25519.pub >> ~/.ssh/authorized_keys
 rm -rf /proj/zyuxuanssf-PG0/docker
 mkdir /proj/zyuxuanssf-PG0/docker && cd /proj/zyuxuanssf-PG0/docker
 wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.6.9-1_amd64.deb
