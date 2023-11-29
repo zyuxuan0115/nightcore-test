69193bf570516e4bd7d3e965b8383cd5e4e6f6f2
zyuxuan <zyuxuan@localhost>
2023-11-28 20:01:32 -0500

    update

--- moviereview_singlenode/docker-files/docker_images.sh
+++ moviereview_singlenode/docker-files/docker_images.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 ROOT_DIR=`realpath $(dirname $0)/..`
+echo $ROOT_DIR
 
 function build_nightcore {
-    docker build -t zjia/nightcore:asplos-ae \
-        -f $ROOT_DIR/dockerfiles/Dockerfile.nightcore \
+    docker build -t zyuxuan0115/nightcore:main \
+        -f $ROOT_DIR/Dockerfile.nightcore \
         $ROOT_DIR/nightcore
 }
 
 function push_nightcore {
-    docker push zjia/nightcore:asplos-ae
+    docker push zyuxuan0115/nightcore:main
 }
 
 function build_deathstarbench {
-    docker build -t zjia/cpp-microservice-deps:asplos-ae \
+    docker build -t zyuxuan0115/cpp-microservice-deps:main \
         --build-arg NUM_CPUS=$(nproc) \
         $ROOT_DIR/workloads/DeathStarBench/socialNetwork/docker/cpp-microservice-deps
 
-    docker build -t zjia/nightcore-moviereview:asplos-ae \
+    docker build -t zjia/nightcore-moviereview:main \
         -f $ROOT_DIR/dockerfiles/Dockerfile.moviereview \
         --build-arg NUM_CPUS=$(nproc) \
         $ROOT_DIR/workloads/DeathStarBench
 }
 
 function push_deathstarbench {
-    docker push zjia/cpp-microservice-deps:asplos-ae
-    docker push zjia/nightcore-moviereview:asplos-ae
+    docker push zyuxuan0115/cpp-microservice-deps:main
+    docker push zyuxuan0115/nightcore-moviereview:main
 }
 
 HIPSTERSHOP_SERVICES="frontend-api \
