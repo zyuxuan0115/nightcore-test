a0fb074c77a6e4343f187c65a714c6c27009b836
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2023-11-28 19:36:05 -0500

    update

---
+++ moviereview_singlenode/docker-files/docker_images.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+ROOT_DIR=`realpath $(dirname $0)/..`
+
+function build_nightcore {
+    docker build -t zjia/nightcore:asplos-ae \
+        -f $ROOT_DIR/dockerfiles/Dockerfile.nightcore \
+        $ROOT_DIR/nightcore
+}
+
+function push_nightcore {
+    docker push zjia/nightcore:asplos-ae
+}
+
+function build_deathstarbench {
+    docker build -t zjia/cpp-microservice-deps:asplos-ae \
+        --build-arg NUM_CPUS=$(nproc) \
+        $ROOT_DIR/workloads/DeathStarBench/socialNetwork/docker/cpp-microservice-deps
+
+    docker build -t zjia/nightcore-moviereview:asplos-ae \
+        -f $ROOT_DIR/dockerfiles/Dockerfile.moviereview \
+        --build-arg NUM_CPUS=$(nproc) \
+        $ROOT_DIR/workloads/DeathStarBench
+}
+
+function push_deathstarbench {
+    docker push zjia/cpp-microservice-deps:asplos-ae
+    docker push zjia/nightcore-socialnetwork:asplos-ae
+    docker push zjia/nightcore-moviereview:asplos-ae
+    docker push zjia/nightcore-hotelresv:asplos-ae
+}
+
+HIPSTERSHOP_SERVICES="frontend-api \
+productcatalogservice \
+shippingservice \
+checkoutservice \
+currencyservice \
+paymentservice \
+adservice \
+recommendationservice \
+cartservice"
+
+function build {
+    build_nightcore
+    build_deathstarbench
+    build_hipstershop
+}
+
+function push {
+    push_nightcore
+    push_deathstarbench
+    push_hipstershop
+}
+
+case "$1" in
+build)
+    build
+    ;;
+push)
+    push
+    ;;
+esac
