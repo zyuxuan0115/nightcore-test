d23d140b1a65b95a9901653d1a31f936575a8545
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2023-12-13 16:47:22 -0500

    update

--- socialnetwork_singlenode/DeathStarBench/socialNetwork/src/UniqueIdService/UniqueIdService.cpp
+++ socialnetwork_singlenode/DeathStarBench/socialNetwork/src/UniqueIdService/UniqueIdService.cpp
@@ -24,24 +24,7 @@ static std::mutex* thread_lock;
 static ClientPool<RedisClient>* redis_client_pool;
 static ClientPool<RabbitmqClient>* rabbitmq_client_pool;
 
-int faas_init() {
-    init_logger();
-    SetUpTracer("config/jaeger-config.yml", "post-storage-service");
-
-    if (load_config(&config_json) != 0) {
-      return -1;
-    }
-
-    if (GetMachineId(&machine_id) != 0) {
-      return -1;
-    }
-
-    thread_lock = new std::mutex;
-
-    return 0;
-}
-
-int faas_init_compose_post(){
+int faas_init_callee(){
   init_logger();
   SetUpTracer("config/jaeger-config.yml", "post-storage-service");
 
@@ -62,6 +45,47 @@ int faas_init_compose_post(){
   return 0;
 }
 
+int faas_init() {
+    init_logger();
+    SetUpTracer("config/jaeger-config.yml", "post-storage-service");
+
+    if (load_config(&config_json) != 0) {
+      return -1;
+    }
+
+    if (GetMachineId(&machine_id) != 0) {
+      return -1;
+    }
+
+    thread_lock = new std::mutex;
+   
+    faas_init_callee();
+
+    return 0;
+}
+
+
+int faas_create_func_worker_callee(void* caller_context,
+                            faas_invoke_func_fn_t invoke_func_fn,
+                            faas_append_output_fn_t append_output_fn,
+                            void** worker_handle, FaasWorker* faas_worker) {
+
+    std::string compose_post_addr = config_json["compose-post-service"]["addr"];
+    int compose_post_port = config_json["compose-post-service"]["port"];
+    std::string compose_post_http_path = config_json["compose-post-service"]["http_path"];
+
+    auto compose_post_client_pool = new ClientPool<ThriftClient<ComposePostServiceClient>>(
+        "compose-post", compose_post_addr, compose_post_port, 0, config_json["unique-id-service"]["compose_post_client_pool_size"],
+        1000, "ComposePostService", faas_worker, "UniqueIdService", "ComposePostService");
+
+    faas_worker->SetProcessor_1(std::make_shared<UniqueIdServiceProcessor>(
+                              std::make_shared<UniqueIdHandler>(
+                              thread_lock, machine_id, compose_post_client_pool)));
+
+    return 0;
+}
+
+
 int faas_create_func_worker(void* caller_context,
                             faas_invoke_func_fn_t invoke_func_fn,
                             faas_append_output_fn_t append_output_fn,
@@ -69,20 +93,25 @@ int faas_create_func_worker(void* caller_context,
     FaasWorker* faas_worker = new FaasWorker(
         caller_context, invoke_func_fn, append_output_fn);
 
-  std::string compose_post_addr = config_json["compose-post-service"]["addr"];
-  int compose_post_port = config_json["compose-post-service"]["port"];
-  std::string compose_post_http_path = config_json["compose-post-service"]["http_path"];
+    std::string compose_post_addr = config_json["compose-post-service"]["addr"];
+    int compose_post_port = config_json["compose-post-service"]["port"];
+    std::string compose_post_http_path = config_json["compose-post-service"]["http_path"];
 
-  auto compose_post_client_pool = new ClientPool<ThriftClient<ComposePostServiceClient>>(
-      "compose-post", compose_post_addr, compose_post_port, 0, config_json["unique-id-service"]["compose_post_client_pool_size"],
-      1000, "ComposePostService", faas_worker, "UniqueIdService", "ComposePostService");
+    auto compose_post_client_pool = new ClientPool<ThriftClient<ComposePostServiceClient>>(
+        "compose-post", compose_post_addr, compose_post_port, 0, config_json["unique-id-service"]["compose_post_client_pool_size"],
+        1000, "ComposePostService", faas_worker, "UniqueIdService", "ComposePostService");
 
     faas_worker->SetProcessor(std::make_shared<UniqueIdServiceProcessor>(
-          std::make_shared<UniqueIdHandler>(
-              thread_lock, machine_id, compose_post_client_pool)));
+                              std::make_shared<UniqueIdHandler>(
+                              thread_lock, machine_id, compose_post_client_pool)));
+
+
+    faas_create_func_worker_callee(caller_context, invoke_func_fn, append_output_fn, worker_handle, faas_worker);
+
     *worker_handle = faas_worker;
     return 0;
 }
+
 
 int faas_destroy_func_worker(void* worker_handle) {
     FaasWorker* faas_worker = reinterpret_cast<FaasWorker*>(worker_handle);
@@ -94,5 +123,12 @@ int faas_func_call(void* worker_handle,
                    const char* input, size_t input_length) {
     FaasWorker* faas_worker = reinterpret_cast<FaasWorker*>(worker_handle);
     bool success = faas_worker->Process(input, input_length);
+    return success ? 0 : -1;
+}
+
+int faas_func_call_callee(void* worker_handle,
+                   const char* input, size_t input_length) {
+    FaasWorker* faas_worker = reinterpret_cast<FaasWorker*>(worker_handle);
+    bool success = faas_worker->Process_1(input, input_length);
     return success ? 0 : -1;
 }
