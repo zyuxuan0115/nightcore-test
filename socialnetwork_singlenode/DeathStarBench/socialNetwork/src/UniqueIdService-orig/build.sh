b4c460cba66dcc85dc6d1ba527fa72e312116d13
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-01-13 15:05:00 -0500

    update

---
+++ socialnetwork_singlenode/DeathStarBench/socialNetwork/src/UniqueIdService-orig/build.sh
@@ -0,0 +1,12 @@
+CUR_DIR=$(pwd)
+#ROOT_DIR="/proj/zyuxuanssf-PG0/nightcore-test/socialnetwork_singlenode/DeathStarBench/socialNetwork"
+ROOT_DIR=$CUR_DIR/../..
+echo $ROOT_DIR
+THRIFT_GEN_CPP_DIR="$ROOT_DIR/gen-cpp"
+CPPFLAGS="-I/usr/local/include/jaegertracing -I$ROOT_DIR/thrift/lib/cpp/src -I$ROOT_DIR/thrift"
+LINKER_FLAGS="-L/usr/local/lib -lboost_log -lboost_log_setup -ljaegertracing" 
+
+g++ -shared -fPIC -O2 $CPPFLAGS  UniqueIdService.cpp $THRIFT_GEN_CPP_DIR/ComposePostService.cpp $THRIFT_GEN_CPP_DIR/social_network_types.cpp $THRIFT_GEN_CPP_DIR/UniqueIdService.cpp -o libUniqueIdService.so $LINKER_FLAGS
+
+#UniqueIdService.o:
+#	g++ -c -fPIC -O2 $(CPPFLAGS)  UniqueIdService.cpp $(THRIFT_GEN_CPP_DIR)/ComposePostService.cpp $(THRIFT_GEN_CPP_DIR)/social_network_types.cpp $(THRIFT_GEN_CPP_DIR)/UniqueIdService.cpp 
