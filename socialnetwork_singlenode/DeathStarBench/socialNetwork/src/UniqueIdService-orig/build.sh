9603c5ddcbbbdc8f97ca481111eb3543c83226ca
zyuxuan <zyuxuan@localhost>
2024-01-13 21:50:56 -0700

    update

--- socialnetwork_singlenode/DeathStarBench/socialNetwork/src/UniqueIdService-orig/build.sh
+++ socialnetwork_singlenode/DeathStarBench/socialNetwork/src/UniqueIdService-orig/build.sh
@@ -7,5 +7,5 @@ THRIFT_GEN_CPP_DIR="$ROOT_DIR/gen-cpp"
 CPPFLAGS="-I/usr/local/include/jaegertracing -I$ROOT_DIR/thrift/lib/cpp/src -I$ROOT_DIR/thrift"
 LINKER_FLAGS="-L/usr/local/lib -lboost_log -lboost_log_setup -ljaegertracing -L/DeathStarBench/socialNetwork/build/lib" 
 
-g++ -shared -fPIC -O2 $CPPFLAGS  UniqueIdService.cpp $THRIFT_GEN_CPP_DIR/ComposePostService.cpp $THRIFT_GEN_CPP_DIR/social_network_types.cpp $THRIFT_GEN_CPP_DIR/UniqueIdService.cpp -o libUniqueIdService.so $LINKER_FLAGS
+g++ -shared -fPIC -O2 $CPPFLAGS  UniqueIdService.cpp $THRIFT_GEN_CPP_DIR/ComposePostService.cpp $THRIFT_GEN_CPP_DIR/social_network_types.cpp $THRIFT_GEN_CPP_DIR/UniqueIdService.cpp -o libUniqueIdService.so /DeathStarBench/socialNetwork/build/lib/libthrift.a
 
