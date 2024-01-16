d72cc60004979bd33020a476667db41d1b18ed56
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-01-16 14:05:47 -0500

    add llvm-link and llc to compile files

--- socialnetwork_singlenode/DeathStarBench/socialNetwork/src/UniqueIdService-man-merge/build.sh
+++ socialnetwork_singlenode/DeathStarBench/socialNetwork/src/UniqueIdService-man-merge/build.sh
@@ -8,6 +8,7 @@ CPPFLAGS="-I/usr/local/include/jaegertracing -I$ROOT_DIR/thrift/lib/cpp/src -I$R
 LINKER_FLAGS="-L/usr/lib/x86_64-linux-gnu -lboost_log -lboost_log_setup -lboost_system -ljaegertracing -L/DeathStarBench/socialNetwork/build/lib" 
 CC=/llvm-project/build/bin/clang++
 LLVM_LINK=/llvm-project/build/bin/llvm-link
+LLC=/llvm-project/build/bin/llc 
 
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS UniqueIdService.cpp -c -o UniqueIdService.ll
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS $THRIFT_GEN_CPP_DIR/ComposePostService.cpp -c -o gen-ComposePostService.ll
@@ -16,6 +17,9 @@ $CC -fPIC -emit-llvm -g -S $CPPFLAGS $THRIFT_GEN_CPP_DIR/UniqueIdService.cpp -c 
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS $THRIFT_GEN_CPP_DIR/PostStorageService.cpp -c -o gen-PostStorageService.ll
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS $THRIFT_GEN_CPP_DIR/UserTimelineService.cpp -c -o gen-UserTimelineService.ll
 $LLVM_LINK UniqueIdService.ll gen-ComposePostService.ll gen-social_network_types.ll gen-UniqueIdService.ll gen-PostStorageService.ll gen-UserTimelineService.ll -o merged.ll -S
+$LLC -filetype=obj -relocation-model=pic merged.ll -o merged.o
+$CC -shared -fPIC -O2 $CPPFLAGS merged.o -o UniqueIdService.so $LINKER_FLAGS
+
 
 $CC -shared -fPIC -O2 $CPPFLAGS  UniqueIdService.cpp $THRIFT_GEN_CPP_DIR/ComposePostService.cpp $THRIFT_GEN_CPP_DIR/social_network_types.cpp $THRIFT_GEN_CPP_DIR/UniqueIdService.cpp $THRIFT_GEN_CPP_DIR/PostStorageService.cpp $THRIFT_GEN_CPP_DIR/UserTimelineService.cpp  -o libUniqueIdService.so $LINKER_FLAGS
 
