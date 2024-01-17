1b1b8dccf03eb0802426f25f4e1e9e40fe3b8694
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-01-16 19:12:27 -0500

    update

--- socialnetwork_singlenode/DeathStarBench/socialNetwork/src/UniqueIdService-man-merge/build.sh
+++ socialnetwork_singlenode/DeathStarBench/socialNetwork/src/UniqueIdService-man-merge/build.sh
@@ -5,17 +5,25 @@ ROOT_DIR="/DeathStarBench"
 THRIFT_GEN_CPP_DIR="$ROOT_DIR/socialNetwork/gen-cpp"
 CPPFLAGS="-I/usr/local/include/jaegertracing -I$ROOT_DIR/thrift/lib/cpp/src -I$ROOT_DIR/socialNetwork/build/thrift -DBOOST_LOG_DYN_LINK"
 LINKER_FLAGS="-L/usr/lib/x86_64-linux-gnu -lboost_log -lboost_log_setup -lboost_system -ljaegertracing -L/DeathStarBench/socialNetwork/build/lib" 
-CC=/llvm-project/build/bin/clang++
-LLVM_LINK=/llvm-project/build/bin/llvm-link
-LLC=/llvm-project/build/bin/llc 
-OPT=/llvm-project/build/bin/opt
+LLVM_BUILD_PATH=/llvm-project/build
+CC=$LLVM_BUILD_PATH/bin/clang++
+LLVM_LINK=$LLVM_BUILD_PATH/bin/llvm-link
+LLC=$LLVM_BUILD_PATH/bin/llc 
+OPT=$LLVM_BUILD_PATH/bin/opt
+MERGE_FUNC_LIB=$LLVM_BUILD_PATH/lib/LLVMMergeFuncReal.so
 
+# compile all source files into IR
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS UniqueIdService.cpp -c -o UniqueIdService.ll
+$CC -fPIC -emit-llvm -g -S $CPPFLAGS $CUR_DIR/../ComposePostService/ComposePostService.cpp -c -o ComposePostService.ll
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS $THRIFT_GEN_CPP_DIR/ComposePostService.cpp -c -o gen-ComposePostService.ll
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS $THRIFT_GEN_CPP_DIR/social_network_types.cpp -c -o gen-social_network_types.ll
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS $THRIFT_GEN_CPP_DIR/UniqueIdService.cpp -c -o gen-UniqueIdService.ll
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS $THRIFT_GEN_CPP_DIR/PostStorageService.cpp -c -o gen-PostStorageService.ll
 $CC -fPIC -emit-llvm -g -S $CPPFLAGS $THRIFT_GEN_CPP_DIR/UserTimelineService.cpp -c -o gen-UserTimelineService.ll
+
+# get all functions in UniqueIdService
+$OPT -load $MERGE_FUNC_LIB -enable-new-pm=0 -ChangeFuncNames -write-func-symbol=func_sym.txt UniqueIdService.ll -S
+
 $LLVM_LINK UniqueIdService.ll gen-ComposePostService.ll gen-social_network_types.ll gen-UniqueIdService.ll gen-PostStorageService.ll gen-UserTimelineService.ll -o merged.ll -S
 $LLC -filetype=obj -relocation-model=pic merged.ll -o merged.o
 $CC -shared -fPIC -O2 $CPPFLAGS merged.o -o libUniqueIdService.so $LINKER_FLAGS
